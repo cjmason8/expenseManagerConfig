71cb9fda35e422de03a18d3982f663e8071be0df
Chris Mason <cjmason8@gmail.com>
2016-09-04 19:50:00 +1000

    add logging

--- build.sh
+++ build.sh
@@ -24,12 +24,14 @@ TAG_NAME=$(<VERSION)
 TAG_NAME="${TAG_NAME%.*}.$((${TAG_NAME##*.}+1))"
 echo $TAG_NAME > VERSION
 
+echo "commiting bump version"
 git config user.name "Release Manager"
 git config user.email "Release.Manager@jenkins.com.au"
 git add --all
 git commit -m "bump version"
 git push https://cjmason8:${GIT_PASS}@github.com/cjmason8/expenseManager.git
 
+echo "login docker"
 docker login --username=cjmason8 --password=$PASSWORD
 
 echo "Beginning cleanup step."
