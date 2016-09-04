a1a6578c046c920a398b8ce16eb2ef3415a89420
Chris Mason <cjmason8@gmail.com>
2016-09-04 19:51:59 +1000

    checkout master

--- build.sh
+++ build.sh
@@ -25,6 +25,7 @@ TAG_NAME="${TAG_NAME%.*}.$((${TAG_NAME##*.}+1))"
 echo $TAG_NAME > VERSION
 
 echo "commiting bump version"
+git checkout master
 git config user.name "Release Manager"
 git config user.email "Release.Manager@jenkins.com.au"
 git add --all
