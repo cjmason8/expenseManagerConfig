0cc3726b8da4a80d6d0b678d683297396424e9d2
Chris Mason <cjmason8@gmail.com>
2016-09-10 11:09:05 +1000

    add logging

--- src/main/java/au/com/mason/expenseManager/TestRestController.java
+++ src/main/java/au/com/mason/expenseManager/TestRestController.java
@@ -1,5 +1,7 @@
 package au.com.mason.expenseManager;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,10 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class TestRestController {
+	
+	private static final Logger LOGGER = LogManager.getLogger(TestRestController.class);
 
 	@CrossOrigin(origins = {"http://54.191.108.166:45612", "http://localhost:45612"})
 	@RequestMapping("/test")
     String home(@RequestParam String type) {
+		LOGGER.info("Entering test");
 		if (type.equals("Joe")) {
 			return "{\"msg\":\"Your the best Joe!\"}";
 		}
