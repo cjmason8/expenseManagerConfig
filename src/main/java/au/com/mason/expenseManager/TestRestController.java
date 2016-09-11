551dd9330d6ed11bbcef18f86f6e5c5c014852ef
Release Manager <Release.Manager@jenkins.com.au>
2016-09-11 11:23:40 +1000

    add logging

--- src/main/java/au/com/mason/expenseManager/TestRestController.java
+++ src/main/java/au/com/mason/expenseManager/TestRestController.java
@@ -1,7 +1,7 @@
 package au.com.mason.expenseManager;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,7 +15,7 @@ class TestRestController {
 	@CrossOrigin(origins = {"http://54.191.108.166:45612", "http://localhost:45612"})
 	@RequestMapping("/test")
     String home(@RequestParam String type) {
-		LOGGER.debug("Entering test");
+		LOGGER.info("Entering test");
 		if (type.equals("Joe")) {
 			return "{\"msg\":\"Your the best Joe!\"}";
 		}
