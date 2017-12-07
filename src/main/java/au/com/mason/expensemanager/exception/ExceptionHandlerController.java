498ab75a1b5bebfc450ae0c0203d81d810f2f0d4
Chris Mason <cjmason8@gmail.com>
2017-12-07 21:32:32 +1100

    add exception handling

---
+++ src/main/java/au/com/mason/expensemanager/exception/ExceptionHandlerController.java
@@ -0,0 +1,22 @@
+package au.com.mason.expensemanager.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+
+	private static Logger LOGGER = LogManager.getLogger(ExceptionHandlerController.class);
+
+    @ExceptionHandler(value = {Exception.class, RuntimeException.class})
+    public String defaultErrorHandler(HttpServletRequest request, Exception e) {
+    	
+    	LOGGER.error("Unhandled Exception - " + request.getContextPath() + ", " + request.getServletPath(), e);
+    	
+    	return "{\"error\":\"failed\"}";
+    }
+}
