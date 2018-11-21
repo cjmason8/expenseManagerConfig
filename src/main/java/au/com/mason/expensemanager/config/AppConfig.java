a249f89ebbef349aee0b91130d8e98bda0006f36
Chris Mason <cjmason8@gmail.com>
2018-11-21 20:00:23 +1100

    fix CORS

--- src/main/java/au/com/mason/expensemanager/config/AppConfig.java
+++ src/main/java/au/com/mason/expensemanager/config/AppConfig.java
@@ -8,8 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class AppConfig implements WebMvcConfigurer {
 
 	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedMethods("PUT", "DELETE", "GET", "POST");
-	}
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
+                .allowCredentials(true);
+    }
 
 }
