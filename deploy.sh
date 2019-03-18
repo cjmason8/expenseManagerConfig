5eec246b1d30a76f7e201b861d01d7ac99e0deb4
Chris Mason <cjmason8@gmail.com>
2019-03-18 20:52:49 +1100

    change all to use env vars

--- deploy.sh
+++ deploy.sh
@@ -11,21 +11,23 @@ TAG_NAME=$(<VERSION)
 
 export TAG_NAME
 export RANCHER_URL
+export RANCHER_ACCESS_KEY
+export RANCHER_SECRET_KEY
+export COMPOSE_PROJECT_NAME
+export COMPOSE_FILE
 
 echo "VER=$TAG_NAME"
-
-echo "TAG_NAME=$TAG_NAME" > .env
 
 echo "Force pulling..."
 rancher-compose pull
 
 echo "Starting deployment..."
-#rancher-compose --url ${RANCHER_URL} --access-key ${RANCHER_ACCESS_KEY} --secret-key ${RANCHER_SECRET_KEY} --rancher-file ${BASE_DIR}/rancher-compose.yml --project-name expenseManager --file ${BASE_DIR}/${ENV_NAME}/docker-compose-${ENV_NAME}.yml up --upgrade -d --pull --batch-size 1
+rancher-compose up --upgrade -d --pull --batch-size 1
 
 if [ $? -eq 0 ]; then
   echo "Deploy success! Confirming..."
-  #rancher-compose --url ${RANCHER_URL} --access-key ${RANCHER_ACCESS_KEY} --secret-key ${RANCHER_SECRET_KEY} --project-name expenseManager --file ${BASE_DIR}/${ENV_NAME}/docker-compose-${ENV_NAME}.yml up --confirm-upgrade -d --batch-size 1
+  rancher-compose up --confirm-upgrade -d --batch-size 1
 else
   echo "Deploy failed :( rolling back..."
-  #rancher-compose --url ${RANCHER_URL} --access-key ${RANCHER_ACCESS_KEY} --secret-key ${RANCHER_SECRET_KEY} --project-name expenseManager --file ${BASE_DIR}/${ENV_NAME}/docker-compose-${ENV_NAME}.yml up --rollback -d --batch-size 1
+  rancher-compose up --rollback -d --batch-size 1
 fi
