1c567757ade095883a2cfd79dee2f274c8e5f049
Chris Mason <cjmason8@gmail.com>
2019-04-10 14:17:17 +1000

    add buildLocal

---
+++ buildLocal.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+FULL_IMAGE_NAME=expense-manager
+TAG_NAME=$(<LOCAL)
+
+echo "Beginning cleanup step."
+echo "Removing docker images for: ${FULL_IMAGE_NAME}"
+set +e
+# Below to be implemented when docker has been updated to >1.10
+#  docker rmi -f $(docker images --format "{{.Repository}}:{{.Tag}}" ${FULL_IMAGE_NAME}) 2> /dev/null
+docker rmi $(docker images | grep "^${FULL_IMAGE_NAME}" | awk "{print $3}") 2> /dev/null
+
+echo "Beginning preparation step."
+if [ -z "${TAG_NAME}" ]; then
+  echo "No tag name defined, unable to continue."
+  exit 1
+fi
+
+TAG_NAME="${TAG_NAME%.*}.$((${TAG_NAME##*.}+1))"
+echo $TAG_NAME > LOCAL
+
+echo "Creating image: ${FULL_IMAGE_NAME}:${TAG_NAME}"
+
+#echo "maven"
+cd ../expenseManager
+mvn clean install
+cd ../expenseManagerConfig
+mkdir -p target
+cp ../expenseManager/target/expensemanager-0.0.1-SNAPSHOT.jar target
+
+docker build --no-cache --pull -t ${FULL_IMAGE_NAME}:${TAG_NAME} .
+
+docker tag ${FULL_IMAGE_NAME}:${TAG_NAME} cjmason8/${FULL_IMAGE_NAME}:${TAG_NAME}
+docker tag ${FULL_IMAGE_NAME}:${TAG_NAME} cjmason8/${FULL_IMAGE_NAME}:latest
+docker push cjmason8/${FULL_IMAGE_NAME}:latest
+docker push cjmason8/${FULL_IMAGE_NAME}:${TAG_NAME}
--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,15 @@
 
 RANCHER_ACCESS_KEY=$1
 RANCHER_SECRET_KEY=$2
-RANCHER_URL=http://80.241.221.122:8080/v2-beta/projects/1a5
-ENV_NAME=prd
+RANCHER_URL=$3
+ENV_NAME=$4
 COMPOSE_PROJECT_NAME=expenseManager
 COMPOSE_FILE=${PWD}/${ENV_NAME}/docker-compose-${ENV_NAME}.yml
 TAG_NAME=$(<VERSION)
+
+if [ $ENV_NAME = "lcl" ]; then
+  TAG_NAME=$(<LOCAL)
+fi
 
 export TAG_NAME
 export RANCHER_URL
@@ -18,15 +22,15 @@ export COMPOSE_FILE
 echo "VER=$TAG_NAME"
 
 echo "Force pulling..."
-rancher-compose pull
+rancher-compose -p ${COMPOSE_PROJECT_NAME} -f ${COMPOSE_FILE} pull
 
 echo "Starting deployment..."
-rancher-compose up --upgrade -d --pull --batch-size 1
+rancher-compose -p ${COMPOSE_PROJECT_NAME} -f ${COMPOSE_FILE} up --upgrade -d --pull --batch-size 1
 
 if [ $? -eq 0 ]; then
   echo "Deploy success! Confirming..."
-  rancher-compose up --confirm-upgrade -d --batch-size 1
+  rancher-compose -p ${COMPOSE_PROJECT_NAME} -f ${COMPOSE_FILE} up --confirm-upgrade -d --batch-size 1
 else
   echo "Deploy failed :( rolling back..."
-  rancher-compose up --rollback -d --batch-size 1
+  rancher-compose -p ${COMPOSE_PROJECT_NAME} -f ${COMPOSE_FILE} up --rollback -d --batch-size 1
 fi
