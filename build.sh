ef53559c9bf18bdff140e50b13e1e848f8941aaf
Chris Mason <cjmason8@gmail.com>
2019-04-12 10:35:32 +1000

    change base maven dir

--- build.sh
+++ build.sh
@@ -19,10 +19,8 @@ fi
 echo "Creating image: ${FULL_IMAGE_NAME}:${TAG_NAME}"
 
 #echo "maven"
-cd expenseManager
 #mvn clean install
-docker run --rm -v "$PWD":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -w /usr/src/mymaven maven:3.6.0-jdk-12-alpine mvn clean install
-cd ..
+docker run --rm -v "../expenseManager":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -w /usr/src/mymaven maven:3.6.0-jdk-12-alpine mvn clean install
 mkdir -p target
 cp expenseManager/target/expensemanager-0.0.1-SNAPSHOT.jar target
 
