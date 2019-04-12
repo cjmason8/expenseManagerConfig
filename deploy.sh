8f119256c4aecd8e69cdd53e1dff1baa847103f0
Chris Mason <cjmason8@gmail.com>
2019-04-12 14:42:07 +1000

    change deploy

--- deploy.sh
+++ deploy.sh
@@ -13,24 +13,19 @@ if [ $ENV_NAME = "lcl" ]; then
 fi
 
 export TAG_NAME
-export RANCHER_URL
-export RANCHER_ACCESS_KEY
-export RANCHER_SECRET_KEY
-export COMPOSE_PROJECT_NAME
-export COMPOSE_FILE
 
 echo "VER=$TAG_NAME"
 
 echo "Force pulling..."
-rancher-compose -p ${COMPOSE_PROJECT_NAME} -f ${COMPOSE_FILE} pull
+rancher-compose -p ${COMPOSE_PROJECT_NAME} -f ${COMPOSE_FILE} --url ${RANCHER_URL} --access-key ${RANCHER_ACCESS_KEY} --secret-key ${RANCHER_SECRET_KEY} pull
 
 echo "Starting deployment..."
-rancher-compose -p ${COMPOSE_PROJECT_NAME} -f ${COMPOSE_FILE} up --upgrade -d --pull --batch-size 1
+rancher-compose -p ${COMPOSE_PROJECT_NAME} -f ${COMPOSE_FILE} --url ${RANCHER_URL} --access-key ${RANCHER_ACCESS_KEY} --secret-key ${RANCHER_SECRET_KEY} up --upgrade -d --pull --batch-size 1
 
 if [ $? -eq 0 ]; then
   echo "Deploy success! Confirming..."
-  rancher-compose -p ${COMPOSE_PROJECT_NAME} -f ${COMPOSE_FILE} up --confirm-upgrade -d --batch-size 1
+  rancher-compose -p ${COMPOSE_PROJECT_NAME} -f ${COMPOSE_FILE} --url ${RANCHER_URL} --access-key ${RANCHER_ACCESS_KEY} --secret-key ${RANCHER_SECRET_KEY} up --confirm-upgrade -d --batch-size 1
 else
   echo "Deploy failed :( rolling back..."
-  rancher-compose -p ${COMPOSE_PROJECT_NAME} -f ${COMPOSE_FILE} up --rollback -d --batch-size 1
+  rancher-compose -p ${COMPOSE_PROJECT_NAME} -f ${COMPOSE_FILE} --url ${RANCHER_URL} --access-key ${RANCHER_ACCESS_KEY} --secret-key ${RANCHER_SECRET_KEY} up --rollback -d --batch-size 1
 fi
