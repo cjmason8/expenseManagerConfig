36b8c73e5a54f26ad0283f3151b6b1221898ff68
Chris Mason <cjmason8@gmail.com>
2019-08-04 16:13:38 +1000

    alter local build

--- buildLocal.sh
+++ buildLocal.sh
@@ -32,5 +32,3 @@ docker build --no-cache --pull -t ${FULL_IMAGE_NAME}:${TAG_NAME} .
 
 docker tag ${FULL_IMAGE_NAME}:${TAG_NAME} cjmason8/${FULL_IMAGE_NAME}:${TAG_NAME}
 docker tag ${FULL_IMAGE_NAME}:${TAG_NAME} cjmason8/${FULL_IMAGE_NAME}:latest
-docker push cjmason8/${FULL_IMAGE_NAME}:latest
-docker push cjmason8/${FULL_IMAGE_NAME}:${TAG_NAME}
